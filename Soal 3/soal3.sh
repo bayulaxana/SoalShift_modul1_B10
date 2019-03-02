36a21882976db0e89654083a5154dadc41df867b
bayulaxana <bynd4spd@gmail.com>
2019-03-02 23:24:49 +0700

    lapres

--- Soal 3/soal3.sh
+++ Soal 3/soal3.sh
@@ -16,11 +16,27 @@ do
     fi
     i=`expr $i + 1`
 done
-makePassword
-passCheck=`grep -w $newPass password*.txt 2> /dev/null`
-while [ ${#passCheck} != 0 ]
+
+while [ true ]
 do
+    num=0
+    lower=0
+    upper=0
+    exist=1
     makePassword
     passCheck=`grep -w $newPass password*.txt 2> /dev/null`
+    if [ ${#passCheck} == 0 ]
+    then
+        exist=0
+    fi
+
+    for (( i=0; i<12; i++ )) 
+    do
+        x=`printf "%d\n" "'${newPass:$i:1}"`
+        if [ $x -ge 97 -a $x -le 122 ]; then lower=1;
+        elif [ $x -ge 65 -a $x -le 90 ]; then upper=1;
+        elif [ $x -ge 48 -a $x -le 57 ]; then num=1; fi
+    done
+    if [ $lower == 1 -a $upper == 1 -a $num == 1 -a $exist == 0 ]; then break; fi
 done
 echo $newPass > $fname
